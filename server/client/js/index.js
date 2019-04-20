8d8333c08ac004493b12f60d9de4a71a1e068b6a
Mike Jagd Winum <skupi1234@gmail.com>
2019-04-20 17:37:01 +0200

    Begun setting up the dynamic product loading

--- server/client/js/index.js
+++ server/client/js/index.js
@@ -1,16 +1,23 @@
 let products;
 
 $(document).ready(function(){
-    fetchServer("products");
+    fetchProducts();
 });
 
-function fetchServer(subject){
+function fetchProducts(){
     let server = "http://localhost:8080/"
-    fetch(server+subject).then(response=>{
+    fetch(server+"products").then(response=>{
         return response.json();
     }).then(myJson=>{
         products = myJson;
+        createProductPanes();
     });
+}
+
+function createProductPanes(){
+    for(i = 0; i < products.length; i++){
+        console.log(products[i]);
+    }
 }
 
 $('.loginBtn').click(function(){
